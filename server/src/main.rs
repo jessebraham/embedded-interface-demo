b7cb29b3d93ad4ce79b18bc661f396a340aaba69
Jesse Braham <jesse@beta7.io>
2021-12-15 15:15:34 -0800

    Configure the WiFi peripheral as a soft AP, define a basic event handler

--- server/src/main.rs
+++ server/src/main.rs
@@ -21,12 +21,9 @@ fn main() -> Result<()> {
     link_patches();
     EspLogger::initialize_default();
 
-    #[allow(unused)]
-    let default_nvs = Arc::new(EspDefaultNvs::new()?);
-    #[allow(unused)]
-    let netif_stack = Arc::new(EspNetifStack::new()?);
-    #[allow(unused)]
-    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+    let _default_nvs = Arc::new(EspDefaultNvs::new()?);
+    let _netif_stack = Arc::new(EspNetifStack::new()?);
+    let _sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
 
     init_soft_ap()?;
 
@@ -34,10 +31,97 @@ fn main() -> Result<()> {
 }
 
 fn init_soft_ap() -> Result<()> {
+    let wifi_init_config = wifi_init_config_default();
+    let mut wifi_config = build_wifi_config()?;
+
+    // Such unsafe wow
+    unsafe {
+        esp_netif_create_default_wifi_ap();
+
+        esp_result!(esp_wifi_init(&wifi_init_config), ())?;
+
+        esp_result!(
+            esp_event_handler_instance_register(
+                WIFI_EVENT,
+                ESP_EVENT_ANY_ID,
+                Some(wifi_event_handler),
+                0 as *mut c_types::c_void,
+                0 as *mut esp_event_handler_instance_t,
+            ),
+            ()
+        )?;
+
+        esp_result!(esp_wifi_set_mode(wifi_mode_t_WIFI_MODE_AP), ())?;
+
+        esp_result!(
+            esp_wifi_set_config(
+                wifi_interface_t_WIFI_IF_AP,
+                &mut wifi_config as *mut wifi_config_t,
+            ),
+            ()
+        )?;
+
+        esp_result!(esp_wifi_start(), ())?;
+    }
+
     info!("WiFi soft AP started");
     info!("SSID: {}\tPASSWORD: {}", WIFI_SSID, WIFI_PASS);
 
     Ok(())
+}
+
+unsafe extern "C" fn wifi_event_handler(
+    _arg: *mut c_types::c_void,
+    _event_base: *const i8,
+    event_id: i32,
+    event_data: *mut c_types::c_void,
+) {
+    #[allow(non_upper_case_globals)]
+    match event_id as u32 {
+        wifi_event_t_WIFI_EVENT_AP_STACONNECTED => {
+            let event = *event_data.cast::<wifi_event_ap_staconnected_t>();
+            info!("New client:  {:#?}", event.mac);
+        }
+        wifi_event_t_WIFI_EVENT_AP_STADISCONNECTED => {
+            let event = *event_data.cast::<wifi_event_ap_stadisconnected_t>();
+            info!("Client left: {:#?}", event.mac);
+        }
+        _ => { /* Ignore all other events for now */ }
+    }
+}
+
+fn wifi_init_config_default() -> wifi_init_config_t {
+    // FIXME: the `osi_funcs` field was previously causing errors using `default()`
+    //        (which is expected), however when setting the fields properly this now
+    //        causes a new error:
+    //
+    //        E (226) esp_image: invalid segment length 0xac02
+    //        E (232) boot: Factory app partition is not bootable
+    //        E (237) boot: No bootable app partitions in the partition table
+    wifi_init_config_t {
+        event_handler: Some(esp_event_send_internal),
+        osi_funcs: unsafe { &mut g_wifi_osi_funcs as *mut wifi_osi_funcs_t },
+        wpa_crypto_funcs: unsafe { g_wifi_default_wpa_crypto_funcs },
+        static_rx_buf_num: CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM as i32,
+        dynamic_rx_buf_num: CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM as i32,
+        tx_buf_type: CONFIG_ESP32_WIFI_TX_BUFFER_TYPE as i32,
+        static_tx_buf_num: WIFI_STATIC_TX_BUFFER_NUM as i32,
+        dynamic_tx_buf_num: WIFI_DYNAMIC_TX_BUFFER_NUM as i32,
+        cache_tx_buf_num: WIFI_CACHE_TX_BUFFER_NUM as i32,
+        csi_enable: WIFI_CSI_ENABLED as i32,
+        ampdu_rx_enable: WIFI_AMPDU_RX_ENABLED as i32,
+        ampdu_tx_enable: WIFI_AMPDU_TX_ENABLED as i32,
+        amsdu_tx_enable: WIFI_AMSDU_TX_ENABLED as i32,
+        nvs_enable: WIFI_NVS_ENABLED as i32,
+        nano_enable: WIFI_NANO_FORMAT_ENABLED as i32,
+        rx_ba_win: WIFI_DEFAULT_RX_BA_WIN as i32,
+        wifi_task_core_id: WIFI_TASK_CORE_ID as i32,
+        beacon_max_len: WIFI_SOFTAP_BEACON_MAX_LEN as i32,
+        mgmt_sbuf_num: WIFI_MGMT_SBUF_NUM as i32,
+        feature_caps: unsafe { g_wifi_feature_caps },
+        sta_disconnected_pm: WIFI_STA_DISCONNECTED_PM_ENABLED != 0,
+        magic: WIFI_INIT_CONFIG_MAGIC as i32,
+    }
 }
 
 fn build_wifi_config() -> Result<wifi_config_t> {
